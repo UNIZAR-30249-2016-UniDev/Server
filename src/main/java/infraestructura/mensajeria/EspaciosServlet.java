faa6898d15e1ef8e88ef91322da5ab4ab261f91d
Eduardo Ibáñez <eduiba94@gmail.com>
2016-04-09 20:20:30 +0200

    desarrollando servlet

--- src/main/java/infraestructura/mensajeria/EspaciosServlet.java
+++ src/main/java/infraestructura/mensajeria/EspaciosServlet.java
@@ -2,12 +2,17 @@ package infraestructura.mensajeria;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dominio.entity.Espacio;
+import infraestructura.persistencia.repositorios.EspacioRepositoryPostgre;
+import infraestructura.services.Espacio2Json;
 
 @WebServlet(value="/espacios", name="EspaciosServlet")
 public class EspaciosServlet extends HttpServlet {
@@ -16,6 +21,7 @@ public class EspaciosServlet extends HttpServlet {
 	private static final String REQ_ESPACIO = "tipo";
 	private static final String REQ_EDIFICIO = "edificio";
 	private static final String REQ_PLANTA = "planta";
+	private static EspacioRepositoryPostgre repository = new EspacioRepositoryPostgre();
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -29,7 +35,7 @@ public class EspaciosServlet extends HttpServlet {
 		handleRequest(req, resp);
 	}
 	
-	private void handleRequest(HttpServletRequest req, HttpServletResponse resp){
+	private void handleRequest(HttpServletRequest req, HttpServletResponse resp) {
 		String response = null;
 		String tipo_espacio = null;
 		String edificio = null;
@@ -40,31 +46,40 @@ public class EspaciosServlet extends HttpServlet {
 		edificio = req.getParameter(REQ_EDIFICIO);
 		strFloor = req.getParameter(REQ_PLANTA);
 		
+		resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		
 		/* Chequear parametros */
-		if (tipo_espacio == null){
-			response = "{ }";	// TODO
-		}
-		
-		if(edificio == null){
-			response = "{ }";	// TODO
-		}
-		
-		if(strFloor == null){
-			response = "{ }";	// TODO
-		} else{
-			if(strFloor.matches("^\\d+$")){
-				// es un entero
-				planta = Integer.parseInt(strFloor);
+		if ((tipo_espacio != null) && (edificio != null) && (strFloor != null)
+				&& strFloor.matches("^\\d+$")) {
+			// es un entero
+			planta = Integer.parseInt(strFloor);
+			List<Espacio> lista = null;
+			switch (tipo_espacio) {
+				case "despachos":
+					lista = repository.findDespachos(planta, edificio);
+			        break;
+		        case "laboratorios":
+		        	lista = repository.findLaboratorios(planta, edificio);	
+		        	break;
+		        case "wcs":
+		        	lista = repository.findWcs(planta, edificio);	
+		        	break;
+		        case "aulas":
+		        	lista = repository.findAulas(planta, edificio);	
+		        	break;
+		        case "todo":
+		        	lista = repository.findAll(planta, edificio);	
+		        	break;
 			}
-			else{
-				response = "{ }";	// TODO
+			if (lista != null) {
+				resp.setStatus(HttpServletResponse.SC_OK);
+				response = "[";
+				for(Espacio espacio: lista) {
+					response += Espacio2Json.espacio2Json(espacio) + ",";
+				}
+				response = response.substring(0, response.length()-1) + "]";
 			}
-		}
-		
-		if(response == null){
-			response = "{ espacio : " + tipo_espacio + ", edificio : " + edificio + ", planta : " + planta + " }";
-		}
-		
+		}	
 		setResponse(response, resp);
 	}
 
