1e6a251f08cae0f7a6cd01d5f6668ef1b3853d09
Eduardo Ibáñez <eduiba94@gmail.com>
2016-04-12 19:19:20 +0200

    cambios x2

--- src/main/java/infraestructura/services/ConexionBD.java
+++ src/main/java/infraestructura/services/ConexionBD.java
@@ -16,7 +16,7 @@ public class ConexionBD {
 	public static boolean iniciarConexion() {
 		try {
 			Class.forName("org.postgresql.Driver");
-			conexion = DriverManager.getConnection("jdbc:postgres://kvwkxeyidjqmms:Ar-RYxpcE6C4kErrimKCb0lXFD@ec2-79-125-126-192.eu-west-1.compute.amazonaws.com:5432/d7984qau8ubrrl");
+			conexion = DriverManager.getConnection("jdbc:postgresql://kvwkxeyidjqmms:Ar-RYxpcE6C4kErrimKCb0lXFD@ec2-79-125-126-192.eu-west-1.compute.amazonaws.com:5432/d7984qau8ubrrl");
 
 			if (conexion != null) {
 				return true;
